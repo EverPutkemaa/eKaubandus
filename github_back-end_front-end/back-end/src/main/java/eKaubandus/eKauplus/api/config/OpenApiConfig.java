36bbb90c5ebd871bc8ba479400061b3056e1cbad
Ever <everputkemaa@gmail.com>
2025-05-04 18:05:25 +0300

    update openApiConfig

--- github_back-end_front-end/back-end/src/main/java/eKaubandus/eKauplus/api/config/OpenApiConfig.java
+++ github_back-end_front-end/back-end/src/main/java/eKaubandus/eKauplus/api/config/OpenApiConfig.java
@@ -1,13 +1,18 @@
 package eKaubandus.eKauplus.api.config;
 
 
-import io.swagger.v3.oas.annotations.info.Contact;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.info.License;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
+
+
+
+
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
