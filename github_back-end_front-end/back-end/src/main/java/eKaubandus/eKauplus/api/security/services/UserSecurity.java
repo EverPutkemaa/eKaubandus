e3e508b5ee4512f63cda00047182ac99794211be
Ever <everputkemaa@gmail.com>
2025-05-04 14:55:13 +0300

    update cors and userSecurity

---
+++ github_back-end_front-end/back-end/src/main/java/eKaubandus/eKauplus/api/security/services/UserSecurity.java
@@ -0,0 +1,19 @@
+package eKaubandus.eKauplus.api.security.services;
+
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component("userSecurity")
+public class UserSecurity {
+
+    public boolean isCurrentUser(Long userId) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null && auth.getPrincipal() instanceof UserDetailsImpl) {
+            UserDetailsImpl userDetails = (UserDetailsImpl) auth.getPrincipal();
+            return userDetails.getId().equals(userId);
+        }
+        return false;
+    }
+}
